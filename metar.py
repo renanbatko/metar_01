34046a1d8d83d625bfb6999efd4f74529caedada
renanbatko <renanbatko@gmail.com>
2017-08-19 20:34:44 -0300

    com erro em algumas localidades

--- metar.py
+++ metar.py
@@ -9,21 +9,21 @@ hoje = hoje.strftime("%d-%m-%Y")
 dia = hoje.split("-", 2)[0]
 mes = hoje.split("-", 2)[1]
 ano = hoje.split("-", 2)[2]
-print dia
-print mes
-print ano
+#print dia
+#print mes
+#print ano
 now = datetime.datetime.now()
 hora = now.hour
-print hora
+#print hora
 url_base = "https://www.ogimet.com/display_metars2.php?lugar="+codigo_oaci+"&tipo=ALL&ord=REV&nil=SI&fmt=html&ano="+str(ano)+"&mes="+str(mes)+"&day="+str(dia)+"&hora="+str(hora)+"&anof="+str(ano)+"&mesf="+str(mes)+"&dayf="+str(dia)+"&horaf="+str(hora)+"&minf=59&enviar=Ver"
-print url_base
-print "aqui ok"
+#print url_base
+#print "aqui ok"
 pagina = urllib.urlopen(url_base)
-print "cheguei aqui"
+#print "cheguei aqui"
 for linha in pagina:
-	print "OK"
 	if (linha.find("<pre>METAR") != -1):
-		print "achei"
-		#metar = linha.split(">")
-		#metar = metar.split("<")
-		#print metar
+		#print "achei"
+		#print linha
+		metar = linha.split("<pre>", 2)[1]
+		metar = metar.split("<", 2)[0]
+		print metar
